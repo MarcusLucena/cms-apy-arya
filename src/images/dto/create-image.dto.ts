7d2b96e96e2818a0c892f144d6ec7c78e149feb1
marcus.lucena.3.glo <marcus.lucena.3@globalhitss.com.br>
2025-03-05 07:10:18 -0300

    feature(Entities): implemented new entities

--- src/images/dto/create-image.dto.ts
+++ src/images/dto/create-image.dto.ts
@@ -1,1 +1,6 @@
-export class CreateImageDto {}
+export class CreateImageDto {
+  url: string;
+  alt: string;
+  title: string;
+  name: string;
+}
--- src/images/entities/image.entity.ts
+++ src/images/entities/image.entity.ts
@@ -1,1 +1,18 @@
-export class Image {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Image {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @Column()
+  alt: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  name: string;
+}
