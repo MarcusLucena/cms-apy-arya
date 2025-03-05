76fccc9215b9be0d973ad9c2a2c3b5947f8698b3
marcus.lucena.3.glo <marcus.lucena.3@globalhitss.com.br>
2025-03-05 10:06:14 -0300

    feature(Environment): implemented environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,21 @@ import { PlatformDataModule } from './platform-data/platform-data.module';
 import { ImagesModule } from './images/images.module';
 import { SettingsModule } from './settings/settings.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [],
       synchronize: true,
     }),
